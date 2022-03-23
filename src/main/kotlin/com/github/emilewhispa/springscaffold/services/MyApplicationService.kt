d0684f9d36f758a156e7ab0e1f62fc97a1b0a0eb
GitHub Action <action@github.com>
2022-03-23 12:57:46 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/emilewhispa/springscaffold/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.emilewhispa.springscaffold.services
+
+import com.github.emilewhispa.springscaffold.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
